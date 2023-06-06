d729dd3101c84712102b16566e3219e183b9224b
Jonatas S. Oliveira <dev.jonatas.oliveira@gmail.com>
2023-06-06 22:37:58 +0000

    FTZ-13: Handled status code

--- lib/src/apis/fytnez/generic_route.dart
+++ lib/src/apis/fytnez/generic_route.dart
@@ -2,8 +2,9 @@ import 'dart:io';
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:fytnez_mobile/src/dtos/generic_dto.dart';
+import 'package:fytnez_mobile/src/views/dialogs/generic_dialogs.dart';
 import 'package:http/http.dart' as http;
+import 'package:fytnez_mobile/src/dtos/generic_dto.dart';
 
 abstract class GenericRoute<DTO extends GenericDTO> {
   final String? _apiRoute = dotenv.env['FYTNEZ_API_URL'];
@@ -20,6 +21,20 @@ abstract class GenericRoute<DTO extends GenericDTO> {
           'Content-Type': 'application/json; charset=UTF-8',
         },
         body: jsonEncode(dto.toMap()));
-    return response.statusCode == HttpStatus.created;
+
+    switch (response.statusCode) {
+      case HttpStatus.created:
+        return true;
+
+      case HttpStatus.badRequest:
+        GenericDialogs.showAlertDialog(
+            'Não foi possível criar: ${response.body}');
+        return false;
+
+      default:
+        GenericDialogs.showAlertDialog(
+            'Não foi possível criar: ${response.body}');
+        return false;
+    }
   }
 }
